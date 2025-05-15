a904febc4d696d10776b5c23747805e416758826
JozefKoleda <dodo.koleda17@gmail.com>
2025-05-15 14:18:22 +0200

    Add empty training functions and training directory

---
+++ src/training/training.py
@@ -0,0 +1,20 @@
+
+
+
+
+def train_epoch_original(epoch_id, dataloader, model, optimizer, loss):
+    return None
+
+
+def eval_epoch_original(epoch_id, dataloader, model, metrics: dict):
+    return None
+
+
+def train_model(start_epoch, end_epoch, model, dataloader_train, dataloader_val, loss, metrics):
+    return None
+
+
+
+
+
+
