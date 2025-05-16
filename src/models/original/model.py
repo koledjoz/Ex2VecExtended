aab762beee22cce95861f699dda7ae0597a0d222
JozefKoleda <dodo.koleda17@gmail.com>
2025-05-16 13:25:55 +0200

    Code reformat

--- src/models/original/model.py
+++ src/models/original/model.py
@@ -7,14 +7,14 @@ class Ex2VecOriginal(torch.nn.Module):
         self.config = config
         self.n_users = config['n_users']
         self.n_items = config['n_items']
-        self.latend_d = config['latent_d']
+        self.latent_d = config['latent_d']
 
         self.global_lamb = torch.nn.Parameter(torch.tensor(1.0))
 
-        self.user_lamb = torch.nn.Embedding(self.n_users +1, 1)
+        self.user_lamb = torch.nn.Embedding(self.n_users + 1, 1)
 
-        self.user_bias = torch.nn.Embedding(self.n_users +1, 1)
-        self.item_bias = torch.nn.Embedding(self.n_items +1, 1)
+        self.user_bias = torch.nn.Embedding(self.n_users + 1, 1)
+        self.item_bias = torch.nn.Embedding(self.n_items + 1, 1)
 
         self.alpha = torch.nn.Parameter(torch.tensor(1.0))
         self.beta = torch.nn.Parameter(torch.tensor(-0.065))
@@ -23,15 +23,14 @@ class Ex2VecOriginal(torch.nn.Module):
         self.cutoff = torch.nn.Parameter(torch.tensor(3.0))
 
         self.embedding_user = torch.nn.Embedding(
-            num_embeddings=self.n_users +1, embedding_dim=self.latend_d
+            num_embeddings=self.n_users + 1, embedding_dim=self.latend_d
         )
 
         self.embedding_item = torch.nn.Embedding(
-            num_embeddings=self.n_items +1, embedding_dim=self.latend_d
+            num_embeddings=self.n_items + 1, embedding_dim=self.latend_d
         )
 
         self.logistic = torch.nn.Sigmoid()
-
 
     def forward(self, user_id, item_id, timedeltas, weights):
         user_emb = self.embedding_user(user_id).unsqueeze(1)
@@ -44,9 +43,7 @@ class Ex2VecOriginal(torch.nn.Module):
 
         lamb = self.global_lamb + self.user_lamb(user_id).unsqueeze(-1)
 
-
         timedeltas = torch.pow(torch.clamp(timedeltas + self.cutoff, min=1e-6), -0.5)
-
 
         timedeltas = timedeltas * weights
 
@@ -58,6 +55,4 @@ class Ex2VecOriginal(torch.nn.Module):
 
         output = torch.maximum(torch.zeros_like(base_dist), base_dist - base_level)
 
-        I = self.alpha * output  + self.beta * torch.pow(output, 2) + self.gamma + u_bias + i_bias
-
-        return I
+        return self.alpha * output + self.beta * torch.pow(output, 2) + self.gamma + u_bias + i_bias
