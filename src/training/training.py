c83b0a5011d4615646d9b117672ee8d9ece351e0
JozefKoleda <dodo.koleda17@gmail.com>
2025-05-17 11:22:32 +0200

    Added function to prepare training, create datalaoders and optimizers, load from checkpoint

--- src/training/training.py
+++ src/training/training.py
@@ -1,5 +1,4 @@
-
-
+import torch
 
 
 def train_epoch_original(epoch_id, dataloader, model, optimizer, loss):
@@ -10,11 +9,45 @@ def eval_epoch_original(epoch_id, dataloader, model, metrics: dict):
     return None
 
 
-def train_model(start_epoch, end_epoch, model, dataloader_train, dataloader_val, loss, metrics):
+def train_model(epochs_done, epoch_count, model, dataloader_train, dataloader_val, loss):
     return None
 
 
+def prepare_training(model, train_data, val_data, checkpoint, train_config):
+    match train_config['optimizer']:
+        case "adam":
+            optimizer = torch.optim.Adam(model.parameters(), lr=train_config['learning_rate'])
+        case "sgd":
+            optimizer = torch.optim.SGD(model.parameters(), lr=train_config['learning_rate'])
+        case _:
+            raise ValueError(f"No such optimizer as {train_config['optimizer']} currently supported.")
 
+    epochs_done = 0
+    epoch_count = train_config['epoch_count']
 
+    if checkpoint is not None:
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epochs_done = checkpoint['epoch']
 
+    losses = []
 
+    for i, loss in enumerate(train_config['loss']):
+        match loss:
+            case 'cross_entropy':
+                losses.append(torch.nn.CrossEntropyLoss())
+            case _:
+                raise ValueError(f"No such loss as {loss} currently supported.")
+
+    with train_config['train'] as config:
+        dataloader_train = torch.utils.data.DataLoader(train_data, batch_size=config['batch_size'],
+                                                       num_workers=config['num_workers'], shuffle=config['shuffle'])
+
+    if val_data is not None:
+        with train_config['val'] as config:
+            dataloader_val = torch.utils.data.DataLoader(val_data, batch_size=config['batch_size'],
+                                                         num_workers=config['num_workers'], shuffle=config['shuffle'])
+    else:
+        dataloader_val = None
+
+    return epochs_done, epoch_count, model, dataloader_train, dataloader_val, losses
