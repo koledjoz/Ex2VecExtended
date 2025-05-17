07a1ebb6d6cb481092b332749a05334839ceece4
JozefKoleda <dodo.koleda17@gmail.com>
2025-05-17 08:26:41 +0200

    Add functions for loading of torch checkpoints

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
 import json
+import torch
 
 
 def load_config(config_path):
     with open(config_path, 'r') as f:
         return json.load(f)
+
+
+def load_checkpoint(checkpoint_path):
+    return torch.load(checkpoint_path, weights_only=True)
